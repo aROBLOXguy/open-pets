0c92ef6dadc3cb906718948c99dc07d1ed85108f
aROBLOXguy <104607379+aROBLOXguy@users.noreply.github.com>
2026-04-21 22:16:53 -0700

    ughhh so many pictures

---
+++ images/error.php
@@ -0,0 +1,10 @@
+<?php require_once("../inc/settings.inc.php");?>
+<center>
+<img src="http://images.neopets.com/pets/happy/quiggle_blue_baby.gif" width="150" height="150" border="0">
+<br>
+<b><?=$branding?> Image Folder</b>
+<p>
+<a href="/">
+(you are in the wrong place, aren't you?  better click here)</a>
+</p>
+</center>
